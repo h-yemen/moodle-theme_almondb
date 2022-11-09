f985c71c34a5f8ef19156df81eab5edfc55d5466
Hüseyin Yemen <yemenhuseyin@gmail.com>
2022-11-09 15:23:15 +0300

    modified:   scss/almondb/dashboard/_blog.scss

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022102200;
+$plugin->version   = 2022110900;
 $plugin->requires  = 2022041200;
 $plugin->component = 'theme_almondb';
