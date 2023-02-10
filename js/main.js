71e30c86be3103e6922f29364bff552cb76e4de3
Hüseyin Yemen <yemenhuseyin@gmail.com>
2023-02-10 17:41:52 +0300

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Theme info
+// Theme info.
 const idvar = document.getElementById("theme-info");
 if (idvar) {
     document.getElementById("theme-info").innerHTML = "Moodle Theme Almondb - Writer Themes Almond";
