3331dee74c78e87a1d96c6708c434245a52c2feb
Hüseyin Yemen <yemenhuseyin@gmail.com>
2023-09-13 12:05:39 +0300

    modified:   version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_almondb';
-$plugin->version   = 2023090200;
+$plugin->version   = 2023091200;
 $plugin->requires  = 2023041800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '420.23.91';
