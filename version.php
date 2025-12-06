44249867db76f5110ad0238b151b5786efa856c1
hyemen <yemenhuseyin@gmail.com>
2025-12-06 21:08:02 +0300

    modified:   version.php

--- version.php
+++ version.php
@@ -23,7 +23,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_almondb';
 $plugin->version = 2025120500;
